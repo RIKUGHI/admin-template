663fb3a8b7b2355da9d8239e3859cc9e51f20732
RikuGhi <rifkhairkhairur@gmail.com>
2023-05-04 08:22:05 +0700

    test table

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,114 @@
+import { FaFilter, FaPlus } from "react-icons/fa"
+import { Header, ManagementLayout } from "../components/organisms"
+import { FormSearch } from "../components/molecules"
+import { Button } from "../components/atoms"
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>
+  return (
+    <ManagementLayout>
+      {/* <Header /> */}
+      <div className="rounded-md bg-white p-5 shadow-md">
+        <div className="flex items-center justify-between">
+          <h3 className="text-lg font-bold">Daftar</h3>
+          <div className="flex space-x-2">
+            <Button style="outlined" icon={FaFilter} name="Filter" />
+            <FormSearch />
+            <Button icon={FaPlus} name="Tambah" />
+          </div>
+        </div>
+
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-left text-sm text-gray-500">
+            <thead className="bg-gray-50 text-xs uppercase text-gray-700">
+              <tr>
+                <th scope="col" className="p-4">
+                  <div className="flex items-center">
+                    <input
+                      id="checkbox-all-search"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500"
+                    />
+                    <label htmlFor="checkbox-all-search" className="sr-only">
+                      checkbox
+                    </label>
+                  </div>
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Product name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Color
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Category
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Accesories
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Available
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Weight
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b bg-white hover:bg-gray-50 ">
+                <td className="w-4 p-4">
+                  <div className="flex items-center">
+                    <input
+                      id="checkbox-table-search-1"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 "
+                    />
+                    <label
+                      htmlFor="checkbox-table-search-1"
+                      className="sr-only"
+                    >
+                      checkbox
+                    </label>
+                  </div>
+                </td>
+                <th
+                  scope="row"
+                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900"
+                >
+                  Apple MacBook Pro 17"
+                </th>
+                <td className="px-6 py-4">Silver</td>
+                <td className="px-6 py-4">Laptop</td>
+                <td className="px-6 py-4">Yes</td>
+                <td className="px-6 py-4">Yes</td>
+                <td className="px-6 py-4">$2999</td>
+                <td className="px-6 py-4">3.0 lb.</td>
+                <td className="flex items-center space-x-3 px-6 py-4">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 hover:underline"
+                  >
+                    Edit
+                  </a>
+                  <a
+                    href="#"
+                    className="font-medium text-red-600 hover:underline"
+                  >
+                    Remove
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </ManagementLayout>
+  )
 }
 
 export default Dashboard
