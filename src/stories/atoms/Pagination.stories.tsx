4778206be6b0eb002e44d321c5462638827c0a32
RikuGhi <rifkhairkhairur@gmail.com>
2023-05-07 17:32:23 +0700

    add pagination story

---
+++ src/stories/atoms/Pagination.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { Pagination } from "../../components/atoms"
+
+const meta: Meta<typeof Pagination> = {
+  title: "Components/Atoms/Pagination",
+  component: Pagination,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof Pagination>
+
+export const Default: Story = {
+  args: {
+    limit: 1,
+    firstIndex: 0,
+    totalData: 1,
+    currentPage: 1,
+    totalPages: 1,
+  },
+}
+
+export const FivePages: Story = {
+  args: {
+    limit: 1,
+    firstIndex: 0,
+    totalData: 1,
+    currentPage: 1,
+    totalPages: 5,
+  },
+}
+
+export const TenPages: Story = {
+  args: {
+    limit: 1,
+    firstIndex: 0,
+    totalData: 1,
+    currentPage: 1,
+    totalPages: 10,
+  },
+}
