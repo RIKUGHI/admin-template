ec5b1c55c9aa654071dd3b8aa5bad6570a277002
RikuGhi <rifkhairkhairur@gmail.com>
2023-05-08 09:45:06 +0700

    update organisms

--- src/components/organisms/index.tsx
+++ src/components/organisms/index.tsx
@@ -1,4 +1,3 @@
 export { default as Navigation } from "./Navigation"
-export { default as Header } from "./Header"
 export { default as AdminLayout } from "./layouts/AdminLayout"
 export { default as ManagementLayout } from "./layouts/ManagementLayout"
