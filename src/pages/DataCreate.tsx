35cb5ac268b96fbcb5267024c5b18f17303ad66c
RikuGhi <rifkhairkhairur@gmail.com>
2023-05-12 05:43:00 +0700

    update input

--- src/pages/DataCreate.tsx
+++ src/pages/DataCreate.tsx
@@ -12,31 +12,10 @@ export default function DataCreate() {
           <div className="space-y-12">
             <div className="border-b border-gray-900/10 pb-12">
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="col-span-full">
-                  <label
-                    htmlFor="about"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    About
-                  </label>
-                  <div className="mt-2">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={3}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    ></textarea>
-                  </div>
-                  <p className="mt-2 text-sm text-red-600">
-                    Write a few sentences about yourself.
-                  </p>
-                </div>
-
                 <div className="col-span-full space-y-2">
                   <label className="block text-sm font-medium leading-6 text-gray-900">
                     About
                   </label>
-                  <Input />
                   <textarea
                     rows={3}
                     className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 transition focus:border-blue-500 focus:ring-blue-500"
@@ -82,6 +61,34 @@ export default function DataCreate() {
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
+                </div>
+
+                <div className="col-span-3 space-y-2">
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    First name v
+                  </label>
+                  <input
+                    type="text"
+                    className="block h-9 w-full rounded-md border border-gray-300 bg-gray-50 p-2 text-sm transition focus:border-blue-500 focus:ring-blue-500"
+                  />
+                  <p className="text-sm text-red-600">My version</p>
+                </div>
+
+                <div className="col-span-3 space-y-2">
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Last name v
+                  </label>
+                  <input
+                    type="text"
+                    className="block h-9 w-full rounded-md border border-gray-300 bg-gray-50 p-2 text-sm transition focus:border-blue-500 focus:ring-blue-500"
+                  />
+                  <p className="text-sm text-red-600">My version</p>
                 </div>
 
                 <div className="sm:col-span-4">
