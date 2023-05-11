033948e14378887072101220e6e35e74d7e43f2d
RikuGhi <rifkhairkhairur@gmail.com>
2023-05-12 06:03:34 +0700

    update some form

--- src/pages/DataCreate.tsx
+++ src/pages/DataCreate.tsx
@@ -27,42 +27,6 @@ export default function DataCreate() {
 
             <div className="border-b border-gray-900/10 pb-12">
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="first-name"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    First name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="first-name"
-                      id="first-name"
-                      autoComplete="given-name"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="last-name"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Last name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="last-name"
-                      id="last-name"
-                      autoComplete="family-name"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
                 <div className="col-span-3 space-y-2">
                   <label
                     htmlFor="first-name"
@@ -70,10 +34,7 @@ export default function DataCreate() {
                   >
                     First name v
                   </label>
-                  <input
-                    type="text"
-                    className="block h-9 w-full rounded-md border border-gray-300 bg-gray-50 p-2 text-sm transition focus:border-blue-500 focus:ring-blue-500"
-                  />
+                  <Input />
                   <p className="text-sm text-red-600">My version</p>
                 </div>
 
@@ -84,29 +45,8 @@ export default function DataCreate() {
                   >
                     Last name v
                   </label>
-                  <input
-                    type="text"
-                    className="block h-9 w-full rounded-md border border-gray-300 bg-gray-50 p-2 text-sm transition focus:border-blue-500 focus:ring-blue-500"
-                  />
+                  <Input />
                   <p className="text-sm text-red-600">My version</p>
-                </div>
-
-                <div className="sm:col-span-4">
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Email address
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
                 </div>
 
                 <div className="sm:col-span-3">
