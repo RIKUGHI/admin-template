ba0f2eb78163455c7b7e245f83ece0e3521aec0d
RikuGhi <rifkhairkhairur@gmail.com>
2023-05-11 09:18:26 +0700

    update management layout

--- src/components/organisms/layouts/ManagementLayout.tsx
+++ src/components/organisms/layouts/ManagementLayout.tsx
@@ -4,35 +4,33 @@ const ManagementLayout: FC<PropsWithChildren> = ({ children }) => {
   const mainRef = useRef<HTMLElement>(null)
 
   const onScroll = () => {
-    if (mainRef.current) {
-      const table =
-        mainRef.current.querySelector<HTMLTableSectionElement>(
-          "table[data-sticky]"
-        )
-      const thead = table?.querySelector("thead")
-
-      if (table && thead) {
-        const actualTableHeight = table.clientHeight - thead.clientHeight
-        const offsetTop = table.offsetTop
-        const scrollTop = Math.floor(mainRef.current.scrollTop)
-        const yTranslation = scrollTop - offsetTop
-
-        if (scrollTop > offsetTop) {
-          thead?.style.setProperty(
-            "transform",
-            `translateY(${
-              yTranslation < actualTableHeight
-                ? yTranslation
-                : actualTableHeight - 1
-            }px)`
-          )
-        } else thead?.style.removeProperty("transform")
-      }
-    }
+    // if (mainRef.current) {
+    //   const table =
+    //     mainRef.current.querySelector<HTMLTableSectionElement>(
+    //       "table[data-sticky]"
+    //     )
+    //   const thead = table?.querySelector("thead")
+    //   if (table && thead) {
+    //     const actualTableHeight = table.clientHeight - thead.clientHeight
+    //     const offsetTop = table.offsetTop
+    //     const scrollTop = Math.floor(mainRef.current.scrollTop)
+    //     const yTranslation = scrollTop - offsetTop
+    //     if (scrollTop > offsetTop) {
+    //       thead?.style.setProperty(
+    //         "transform",
+    //         `translateY(${
+    //           yTranslation < actualTableHeight
+    //             ? yTranslation
+    //             : actualTableHeight - 1
+    //         }px)`
+    //       )
+    //     } else thead?.style.removeProperty("transform")
+    //   }
+    // }
   }
 
   return (
-    <main ref={mainRef} className="ml-60 space-y-5 p-5" onScroll={onScroll}>
+    <main ref={mainRef} className="ml-60 space-y-5 p-6" onScroll={onScroll}>
       {children}
     </main>
   )
