ec3c4bc67ab7ac72e903b0a524afb0a21b13415b
RikuGhi <rifkhairkhairur@gmail.com>
2023-05-13 12:14:32 +0700

    add textarea's story

---
+++ src/stories/components/TextArea.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { TextArea } from "../../components/atoms"
+
+const meta: Meta<typeof TextArea> = {
+  title: "Components/TextArea",
+  component: TextArea,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof TextArea>
+
+export const Default: Story = {
+  args: {},
+}
+
+export const WithError: Story = {
+  args: {
+    error: true,
+  },
+}
