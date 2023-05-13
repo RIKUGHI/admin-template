290f5cdf127fd2a3462a7dba91752541ce112383
RikuGhi <rifkhairkhairur@gmail.com>
2023-05-13 10:40:43 +0700

    update radio

--- src/components/atoms/Radio.tsx
+++ src/components/atoms/Radio.tsx
@@ -1,9 +1,19 @@
-const Radio = () => {
+interface Props {
+  name: string
+}
+
+const Radio: React.FC<Props> = ({ name }) => {
   return (
-    <input
-      type="radio"
-      className="h-4 w-4 border-gray-300 bg-gray-100 text-green-600 focus:ring-2 focus:ring-green-500"
-    />
+    <div className="flex items-center">
+      <input
+        id="radio"
+        type="radio"
+        className="h-4 w-4 border-gray-300 bg-gray-100 text-green-600 focus:ring-2 focus:ring-green-500"
+      />
+      <label htmlFor="radio" className="ml-2 text-sm font-medium text-gray-900">
+        {name}
+      </label>
+    </div>
   )
 }
 
