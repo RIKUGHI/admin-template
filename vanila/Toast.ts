47e0561ca73c4adf3d7c1eace269f176dd69e39c
RikuGhi <rifkhairkhairur@gmail.com>
2023-06-07 20:31:02 +0700

    update toast

--- vanila/Toast.ts
+++ vanila/Toast.ts
@@ -1,30 +1,35 @@
-interface IToast {
-  position?: string
-}
-
 interface ToastOptions {
-  position?: string
-  text: string
+  text?: string
+  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right"
+  autoClose?: boolean | number
+  onClose?: () => void
 }
 
-export default class Toast implements IToast {
+const DEFAULT_OPTIONS: ToastOptions = {
+  text: "Hello",
+  position: "top-left",
+  autoClose: 5000,
+}
+
+export default class Toast {
   #toastEl: HTMLDivElement
   #autoCloseTimeout: NodeJS.Timeout | null = null
 
   constructor(options: ToastOptions) {
     this.#toastEl = document.createElement("div")
     this.#toastEl.classList.add("toast")
-
-    Object.entries(options).forEach(([key, value]) => {
-      this[key] = value
-    })
+    this.update({ ...DEFAULT_OPTIONS, ...options })
   }
 
   set position(value: string) {
+    const currentContainer = this.#toastEl.parentElement
     const container =
       document.querySelector(`.toast-container[data-position="${value}"]`) ||
       createContainer(value)
     container.append(this.#toastEl)
+
+    if (currentContainer && !currentContainer.hasChildNodes())
+      currentContainer.remove()
   }
 
   set text(value: string) {
@@ -35,13 +40,24 @@ export default class Toast implements IToast {
     if (typeof value === "boolean" && !value) return
     if (typeof value !== "number") return
 
-    if (this.#autoCloseTimeout) clearTimeout(this.#autoCloseTimeout)
+    if (this.#autoCloseTimeout != null) clearTimeout(this.#autoCloseTimeout)
     this.#autoCloseTimeout = setTimeout(() => this.remove(), value)
+  }
+
+  // set onClose(value) {
+  //   this.onClose = value
+  // }
+
+  update(options: ToastOptions) {
+    Object.entries(options).forEach(([key, value]) => {
+      this[key] = value
+    })
   }
 
   remove() {
     const container = this.#toastEl.parentElement
     this.#toastEl.remove()
+    this.onClose()
     if (container && !container.hasChildNodes()) container.remove()
   }
 }
