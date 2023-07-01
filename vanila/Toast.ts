7067c7a350f84b1b1c9cc68759e687077711008e
RikuGhi <rifkhairkhairur@gmail.com>
2023-07-01 13:02:09 +0700

    update toast

--- vanila/Toast.ts
+++ vanila/Toast.ts
@@ -1,64 +1,170 @@
 interface ToastOptions {
   text?: string
-  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right"
+  position?:
+    | "top-left"
+    | "top-right"
+    | "bottom-left"
+    | "bottom-right"
+    | "top-center"
+    | "bottom-center"
   autoClose?: boolean | number
+  closeOnClick?: boolean
   onClose?: () => void
+  showProgress?: boolean
+  pauseOnHover?: boolean
+  pauseOnFocusLoss?: boolean
 }
 
 const DEFAULT_OPTIONS: ToastOptions = {
-  text: "Hello",
-  position: "top-left",
-  autoClose: 5000,
+  text: "🔥Wow so easy!🔥",
+  position: "top-right",
+  autoClose: 1000,
+  closeOnClick: true,
+  showProgress: true,
+  pauseOnHover: true,
+  pauseOnFocusLoss: true,
 }
 
 export default class Toast {
   #toastEl: HTMLDivElement
-  #autoCloseTimeout: NodeJS.Timeout | null = null
+  #autoCloseInterval: number
+  #progressInterval: number
+  #timeVisible = 0
+  #autoClose = 0
+  #isPaused = false
+  #shouldUnPause: boolean = false
+  #onClose: () => void | undefined
 
   constructor(options: ToastOptions) {
     this.#toastEl = document.createElement("div")
     this.#toastEl.classList.add("toast")
+    requestAnimationFrame(() => {
+      this.#toastEl.classList.add("show")
+    })
     this.update({ ...DEFAULT_OPTIONS, ...options })
   }
 
-  set position(value: string) {
-    const currentContainer = this.#toastEl.parentElement
-    const container =
-      document.querySelector(`.toast-container[data-position="${value}"]`) ||
-      createContainer(value)
-    container.append(this.#toastEl)
-
-    if (currentContainer && !currentContainer.hasChildNodes())
-      currentContainer.remove()
-  }
-
-  set text(value: string) {
-    this.#toastEl.textContent = value
-  }
-
-  set autoClose(value: boolean | number) {
-    if (typeof value === "boolean" && !value) return
-    if (typeof value !== "number") return
-
-    if (this.#autoCloseTimeout != null) clearTimeout(this.#autoCloseTimeout)
-    this.#autoCloseTimeout = setTimeout(() => this.remove(), value)
-  }
-
-  // set onClose(value) {
-  //   this.onClose = value
-  // }
-
   update(options: ToastOptions) {
     Object.entries(options).forEach(([key, value]) => {
-      this[key] = value
+      this[key](value)
     })
   }
 
-  remove() {
+  private text(value: string) {
+    this.#toastEl.textContent = value
+  }
+
+  private position(value: string) {
+    const currentContainer = this.#toastEl.parentElement
+    const container =
+      document.querySelector(`.toast-container[data-position="${value}"]`) || createContainer(value)
+    container.append(this.#toastEl)
+
+    if (currentContainer && !currentContainer.hasChildNodes()) currentContainer.remove()
+  }
+
+  private autoClose(value: boolean | number) {
+    if (typeof value === "number") this.#autoClose = value
+    this.#timeVisible = 0
+    if (typeof value === "boolean" && !value) return
+
+    let lastTime: number | null
+    const frameRequest = (time: number) => {
+      if (this.#shouldUnPause) {
+        lastTime = null
+        this.#shouldUnPause = false
+      }
+
+      if (lastTime == null) {
+        lastTime = time
+        this.#autoCloseInterval = requestAnimationFrame(frameRequest)
+        return
+      }
+
+      if (!this.#isPaused) {
+        this.#timeVisible += time - lastTime
+
+        if (this.#timeVisible >= this.#autoClose) {
+          this.remove()
+          return
+        }
+      }
+
+      lastTime = time
+      this.#autoCloseInterval = requestAnimationFrame(frameRequest)
+    }
+
+    this.#autoCloseInterval = requestAnimationFrame(frameRequest)
+  }
+
+  private closeOnClick(value) {
+    this.#toastEl.classList.toggle("can-close", value)
+    if (value) {
+      this.#toastEl.addEventListener("click", this.remove.bind(this))
+    } else {
+      this.#toastEl.removeEventListener("click", this.remove.bind(this))
+    }
+  }
+
+  private showProgress(value: boolean) {
+    if (!value) return
+
+    this.#toastEl.classList.toggle("progress", value)
+    this.#toastEl.style.setProperty("--progress", "1")
+
+    const frameRequest = () => {
+      if (!this.#isPaused) {
+        this.#toastEl.style.setProperty(
+          "--progress",
+          (1 - this.#timeVisible / this.#autoClose).toString()
+        )
+      }
+      this.#progressInterval = requestAnimationFrame(frameRequest)
+    }
+
+    this.#progressInterval = requestAnimationFrame(frameRequest)
+  }
+
+  private pauseOnHover(value) {
+    if (value) {
+      this.#toastEl.addEventListener("mouseover", () => (this.#isPaused = true))
+      this.#toastEl.addEventListener("mouseleave", () => (this.#isPaused = false))
+    } else {
+      this.#toastEl.removeEventListener("mouseover", () => (this.#isPaused = true))
+      this.#toastEl.removeEventListener("mouseleave", () => (this.#isPaused = false))
+    }
+  }
+
+  private pauseOnFocusLoss(value) {
+    if (value) {
+      document.addEventListener(
+        "visibilitychange",
+        () => (this.#shouldUnPause = document.visibilityState === "visible")
+      )
+    } else {
+      document.removeEventListener(
+        "visibilitychange",
+        () => (this.#shouldUnPause = document.visibilityState === "visible")
+      )
+    }
+  }
+
+  private onClose(cb: () => void) {
+    this.#onClose = cb
+  }
+
+  private remove() {
+    cancelAnimationFrame(this.#autoCloseInterval)
+    cancelAnimationFrame(this.#progressInterval)
+
     const container = this.#toastEl.parentElement
-    this.#toastEl.remove()
-    this.onClose()
-    if (container && !container.hasChildNodes()) container.remove()
+    this.#toastEl.classList.remove("show")
+    this.#toastEl.addEventListener("transitionend", () => {
+      this.#toastEl.remove()
+      if (container && !container.hasChildNodes()) container.remove()
+    })
+
+    if (typeof this.#onClose === "function") this.#onClose()
   }
 }
 
